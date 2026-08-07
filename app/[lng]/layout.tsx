4e747f714b9b75411965daef87a2a6e58e588e4c
cgdiazv <cgdiazv@gmail.com>
2026-08-07 18:41:55 -0500

    Change home page title

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    default: "Prado | Streamlined Operations & Job Management",
-    template: "%s | Prado",
+    default: "Prado Jobs | Streamlined Operations & Job Management",
+    template: "%s | Prado Jobs",
   },
   description: "The all-in-one workspace built for modern service and landscaping professionals. Schedule jobs, manage team workflows, track customers, and simplify billing seamlessly.",
 };
